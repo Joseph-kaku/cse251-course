03af4b032697526aa35dd089f40d39abcdf2b88a
Joseph-kaku <samkaks18@gmail.com>
2023-05-04 22:19:44 -0600

    assignment started

--- week03/team/team.py
+++ week03/team/team.py
@@ -146,9 +146,10 @@ class Board():
         print(f'Finding {word}...')
         for row in range(self.size):
             for col in range(self.size):
-                for d in range(0, 8):
-                    if self._word_at_this_location(row, col, d, word):
-                        return True
+                if word[0] == self.board[row][col]:
+                    for d in range(0, 8):
+                        if self._word_at_this_location(row, col, d, word):
+                            return True
         return False
 
 
